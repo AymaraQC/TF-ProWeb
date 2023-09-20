5218d1236886f35e79a8d8001894e800c9452d4c
AymaraQC <qcaymara18@gmail.com>
2023-09-20 17:09:53 -0300

    rick and martin

---
+++ rickAndMorty-Api/src/App.jsx
@@ -0,0 +1,14 @@
+import { Character } from "./Componentes/Character/Character";
+
+function App() {
+
+
+  return( <>
+      <Character/>
+  
+  </>
+  );
+   
+}
+
+export default App;
---
+++ rickAndMorty-Api/src/Componentes/Character/Character.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import "./character.css";
+
+export function Character() {
+const [ character, setCharacter ] = useState([]);
+
+
+// useEffect y useState Hooks
+useEffect(() => {
+fetch("https://rickandmortyapi.com/api/character")
+.then((response) => response.json())
+.then((data) => setCharacter(data.results));
+}, [setCharacter]);
+//useEffect recibe una funcion callback, es decir el codigo a ejecutar
+// recibe cuando se va a ejecutar
+
+
+return (
+<ul>
+{character.map((item, index) => (
+<li key={index}>
+<h3>{item.name}</h3>
+<p>{item.status}</p>
+<img src={item.image} alt="" />
+<p>{item.species}</p>
+<h4> Last Know Location:</h4>
+<p>{item.location["name"]}</p>
+</li>
+))}
+</ul>
+);
+}
+
+
+
+
+
+
+
+
+/*
+import { useEffect, useState } from "react";
+
+export function Character(){
+const [ character, setCharacter ] = useState([]);
+
+
+
+    //kooks useeFfect(RESIVE DOS PARAMETROS) y useState
+    //el useEffect se ejecurta una vez
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+        .then(data => console.log(data));
+    },[])
+//useEffect recibe una funcion callback, es decir el codigo e ejetutarse
+//recibe cuando se va a ejecutar (todos los datos que van a cambiar en tiempo)
+
+    return(
+
+        <>character</>
+    );
+}
+
+*/
+
+
+
+//useState guarda los datos del anterior
+
